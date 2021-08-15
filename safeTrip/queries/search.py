2649b330bdbb8970c2902c2625bad629865dc5b7
adityabhimesha <aditya.bhimesha456@gmail.com>
2021-08-15 19:29:22 +0530

    backend init

---
+++ safeTrip/queries/search.py
@@ -0,0 +1,59 @@
+exports.searchFirstQuery = `
+      query($name:String!){
+        search(string: $name, network: bsc) {
+          subject {
+            __typename
+            ... on Address {
+              address
+              annotation
+            }
+            ... on Currency {
+              symbol
+              name
+              address
+              tokenType
+              decimals
+            }
+            ... on SmartContract {
+              address
+              annotation
+              contractType
+              protocol
+            }
+          }
+          network {
+            network
+          }
+        }
+      }`
+
+exports.searchSecondQuery = 
+`
+    query($arr:[String!]){
+        ethereum(network: bsc) {
+          dexTrades(
+            options: {limit: 25, desc: "count"}
+            baseCurrency: {in: $arr}
+          ) {
+            smartContract {
+              address {
+                address
+              }
+            }
+            count
+            exchange {
+              name
+            } 
+            baseCurrency {
+              address
+              symbol
+            } 
+            quoteCurrency {
+              address
+              symbol
+            } 
+          }
+        }
+      }
+      
+    `
